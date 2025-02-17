dcccc00b7bc9a9cdae54b7d20801c399653d439f
William Millet <57897661+WilliamMillet@users.noreply.github.com>
2025-02-17 16:05:25 +1100

    Changed to hash router!

--- src/index.js
+++ src/index.js
@@ -9,10 +9,6 @@ import "@fontsource/noto-sans/800.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <BrowserRouter basename='twitter_frontend'>
       <App />
-    </BrowserRouter>
-
 );
 
